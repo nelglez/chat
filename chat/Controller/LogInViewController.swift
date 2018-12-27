621e8b8d17c09cea4662a90cc635dce07f585508
Nelson Gonzalez <nelsongonzalez@nelsons-macbook-pro.local>
2018-12-27 11:01:49 -0500

    adding the register button to the view and adding constraints

--- chat/Controller/LogInViewController.swift
+++ chat/Controller/LogInViewController.swift
@@ -15,8 +15,23 @@ class LogInViewController: UIViewController {
         view.backgroundColor = .white
         //need below for it to actually show up on the screen
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        //if we dont do below cornerRadius will not work.
+        view.mask?.clipsToBounds = true
         return view
     }()
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +40,9 @@ class LogInViewController: UIViewController {
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
       
         view.addSubview(inputContainerView)
+        view.addSubview(loginRegisterButton)
         setupInputContainerView()
+        setupLoginRegisterButton()
         
     }
     
@@ -38,6 +55,17 @@ class LogInViewController: UIViewController {
         inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true //-24 means 12 pixels on the left and 12 pixels on the right
         
         inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func setupLoginRegisterButton(){
+        //need x, y, width, and height
+        //x at center of view
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //y value at bottom of inputContainerView
+        loginRegisterButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 12).isActive = true
+        //set as wide as inputContainerView
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
