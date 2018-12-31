585fc54e38c499fc9e22602b67be9a8853dfab41
Nelson Gonzalez <nelsongonzalez@nelsons-macbook-pro.local>
2018-12-31 15:03:46 -0500

    Displaying users and their email in the tableVC, adding custom tableVC class and registering its identifier.

--- chat/Controller/NewMessageTableViewController.swift
+++ chat/Controller/NewMessageTableViewController.swift
@@ -7,13 +7,38 @@
 //
 
 import UIKit
+import Firebase
 
 class NewMessageTableViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    var users = [Users]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handelCancel))
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        fetchUser()
+    }
+    
+    func fetchUser(){
+        Database.database().reference().child("users").observe(.childAdded) { (snapshot) in
+            if let dict = snapshot.value as? [String: AnyObject] {
+            let name = dict["Name"] as? String
+            let email = dict["Email"] as? String
+           let user = Users()
+                user.name = name
+                user.email = email
+               self.users.append(user)
+                
+                //This will crash because of background thread
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            }
+        }
     }
     
     @objc func handelCancel(){
@@ -25,13 +50,18 @@ class NewMessageTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return users.count
     }
 
    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
 
+       
+        let user = users[indexPath.row]
+         cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
+        
         // Configure the cell...
 
         return cell
@@ -49,4 +79,14 @@ class NewMessageTableViewController: UITableViewController {
     }
     */
 
+}
+
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    required init?(coder aDecoder: NSCoder){
+        fatalError("Error!")
+        
+    }
 }
---
+++ chat/Model/Users.swift
@@ -0,0 +1,16 @@
+//
+//  Users.swift
+//  chat
+//
+//  Created by Nelson Gonzalez on 12/31/18.
+//  Copyright © 2018 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class Users: NSObject {
+
+    var name: String?
+    var email: String?
+    
+}
